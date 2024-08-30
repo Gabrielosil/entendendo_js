2ef8fe82146369da30584597c8f6d44bb9a616f4
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2024-08-29 21:23:58 -0300

    Feat: Finalização dos exercicios 13 e 14

---
+++ exercicio13.js
@@ -0,0 +1,15 @@
+/*Faça um algoritmo que leia um nome de usuário e sua senha.
+A senha não pode ser igual ao nome do usuário,
+uma mensagem de erro e entregando a pedir as informações. */
+let usuario, senha = "";
+do {
+    usuario = prompt("Informe o nome do usuário:");
+    senha = prompt("Informe a sua senha:");
+
+    if (usuario == senha)
+        alert("O usuário não pode ser igual a senha!!");
+
+} while (usuario == senha);
+
+alert("Cadastrado com sucesso!")
+
