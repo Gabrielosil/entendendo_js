60ab3e3f9a54cc294e352f4e1734f594daeb91c1
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2024-09-17 19:50:01 -0300

    .

--- exercicio6.js
+++
@@ -1,9 +0,0 @@
-let nome_do_usuario = (prompt("Digite o seu nome"));
-
-let ganho_por_hora = parseFloat(prompt("Digite o Quanto você ganha por hora"));
-let horas_trabalhadas = parseFloat(prompt("Digite suas horas trabalhadas"));
-let renda_total = ganho_por_hora * horas_trabalhadas
-
-let mensagem = nome_do_usuario + " Recebe " + renda_total.toFixed(2) + " de renda total por mês."
-
-alert(mensagem)
