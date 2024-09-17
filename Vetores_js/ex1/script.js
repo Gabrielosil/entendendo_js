6f9f263b259f90ddafb5a4000f6d2c7550184106
Gabrielosil <gabriel.osilva59@senacsp.edu.br>
2024-09-17 19:51:20 -0300

    fix

---
+++ Vetores_js/ex1/script.js
@@ -0,0 +1,11 @@
+/*Ex1: Crie um sistema de cadastro de funcionários usando funções onde devem ser lidos os nomes, idades e salários de até 
+100 pessoas considerando os seguintes critérios:
+
+• Considere que o usuário não pode informar conteúdo vazio.
+• Nomes completos válidos (nome e sobrenome). 
+• Idades válidas e maiores de 14 anos e menores de 120 anos.
+• Os salários informados devem ser válidos e não podem ser inferiores a R$ 2000,00 e acima de 20000,00.
+• Deve-se cadastrar no mínimo 5 pessoas.
+Exiba todos os cadastrados na página HTML.
+*/
+
